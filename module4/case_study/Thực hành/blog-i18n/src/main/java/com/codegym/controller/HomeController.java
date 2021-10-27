de3b3ea2bf3eb77d4e761955312853dbad74c9ae
tddh2001 <73831180+tddh2001@users.noreply.github.com>
2021-10-27 18:16:26 +0700

    update

---
+++ module4/case_study/Thực hành/blog-i18n/src/main/java/com/codegym/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.codegym.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.codegym.model.Credential;
+import com.codegym.model.User;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(@ModelAttribute("credential") Credential credential){
+        return "index";
+    }
+
+    @PostMapping("/login")
+    public String dashboard(@ModelAttribute("credential") Credential credential, Model model) {
+        User user = new User();
+        user.setUserName(credential.getUsername());
+        model.addAttribute("user", user);
+        return "redirect:dashboard";
+    }
+}
---
+++ module4/case_study/Thực hành/blog-i18n/src/main/java/com/codegym/model/User.java
@@ -0,0 +1,16 @@
+package com.codegym.model;
+
+public class User {
+    private String userName;
+
+    public User() {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
