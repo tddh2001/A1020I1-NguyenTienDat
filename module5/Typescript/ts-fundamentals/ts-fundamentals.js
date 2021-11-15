d223fa3a68cd3aa1f90723011137ae64679156ac
tddh2001 <73831180+tddh2001@users.noreply.github.com>
2021-11-15 21:32:55 +0700

    typescript

---
+++ module5/Typescript/ts-fundamentals/ts-fundamentals.js
@@ -0,0 +1,61 @@
+(function main() {
+    console.log("START");
+    if (true) {
+        var lang = "vi";
+        var target = "en-us";
+        console.log("inside block");
+        console.log(target);
+    }
+    console.log(lang);
+    // console.log(target);
+})();
+(function main() {
+    var x = 5;
+    console.log(x);
+    var x = 10;
+    console.log(x);
+    var y = 55;
+    console.log(y);
+    // let y = 100;
+    // console.log(y);
+})();
+(function main() {
+    console.log(x);
+    var x = 5;
+    // console.log(y);
+    // let y = 10;
+})();
+(function main() {
+    var message;
+    var total = 100;
+    var isProduction = true;
+    var prices = [120, 88, 60];
+    var languages = ['vi', 'en-us'];
+    var now = new Date();
+    var unknown;
+    var Direction;
+    (function (Direction) {
+        Direction[Direction["UP"] = 0] = "UP";
+        Direction[Direction["DOWN"] = 1] = "DOWN";
+        Direction[Direction["LEFT"] = 2] = "LEFT";
+        Direction[Direction["RIGHT"] = 3] = "RIGHT";
+    })(Direction || (Direction = {}));
+    ;
+    function log(msg) {
+        console.log(msg);
+    }
+    isProduction = false;
+    unknown = Direction.UP;
+    unknown = 'changed';
+    // const post: IPost = {};
+    // message = 50;
+    function getPost(postId) {
+        // do something to retrieve post
+        return {
+            id: postId,
+            title: 'Post Title',
+            body: 'Post Body',
+            extra: 'data'
+        };
+    }
+})();
